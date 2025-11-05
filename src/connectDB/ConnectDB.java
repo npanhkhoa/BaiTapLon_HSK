5ce569eee6700c7d29f80a38274c9d4a85b379a7
HaoMai4 <anhdien12121@gmail.com>
2025-11-06 02:50:15 +0700

    Fix connect db

--- src/connectDB/ConnectDB.java
+++ src/connectDB/ConnectDB.java
@@ -5,47 +5,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectDB {
-    private static Connection con = null;
-    private static final ConnectDB instance = new ConnectDB();
+	public static Connection con = null;
+	private static final ConnectDB instance = new ConnectDB();
 
-    private ConnectDB() {
-    }
+	public static ConnectDB getInstance() {
+		return instance;
+	}
 
-    public static ConnectDB getInstance() {
-        return instance;
-    }
+	public static Connection getConnection() {
+		return con;
+	}
 
-    public Connection getConnection() {
-        return con;
-    }
+	public void connect() throws SQLException {
+		String url = "jdbc:sqlserver://localhost:1433;databaseName=QUANLIQUANCAPHE";
+		String user = "sa";
+		String password = "sapassword";
+		con = DriverManager.getConnection(url, user, password);
+	}
 
-    public void connect() throws SQLException {
-        String url = "jdbc:sqlserver://localhost:1433;databaseName=QUANLIQUANCAPHE;encrypt=false";
-        String user = "sa";
-        String password = "sapassword";
-
-        try {
-            if (con == null || con.isClosed()) {
-                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-                con = DriverManager.getConnection(url, user, password);
-                System.out.println("Kết nối SQL Server thành công!");
-            }
-        } catch (ClassNotFoundException e) {
-            System.err.println("Không tìm thấy driver JDBC SQL Server!");
-        } catch (SQLException e) {
-            System.err.println("Lỗi kết nối SQL Server: " + e.getMessage());
-            throw e; // ném ra để tầng gọi xử lý
-        }
-    }
-
-    public void disconnect() {
-        try {
-            if (con != null && !con.isClosed()) {
-                con.close();
-                System.out.println("Đã đóng kết nối SQL Server.");
-            }
-        } catch (SQLException e) {
-            System.err.println("Lỗi khi đóng kết nối: " + e.getMessage());
-        }
-    }
+	public void disconnect() throws SQLException {
+		try {
+			if (con != null) {
+				con.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
