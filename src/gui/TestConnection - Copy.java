5b3dbe12ae588c59b1fddb989dac2b709eddd330
HaoMai4 <anhdien12121@gmail.com>
2025-11-06 15:33:27 +0700

    Add test branch

---
+++ src/gui/TestConnection - Copy.java
@@ -0,0 +1,28 @@
+package gui;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import connectDB.ConnectDB;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        try {
+            ConnectDB.getInstance().connect();
+            ConnectDB.getInstance();
+			Connection con = ConnectDB.getConnection();
+
+            String sql = "SELECT TOP 5 maSanPham, tenSanPham FROM SanPham";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("maSanPham") + " - " + rs.getString("tenSanPham"));
+            }
+
+            ConnectDB.getInstance().disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
